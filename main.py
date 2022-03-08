c68198dec19777dea06efd57cdc1496d4b3d7292
Jesse Kinsella <89218189+JesseKinsella@users.noreply.github.com>
2022-03-08 18:48:29 +0000

    added code

---
+++ main.py
@@ -0,0 +1,55 @@
+#For loop allows you to execute a piece of code over and over again
+
+#for number in range(10):
+#  print("Hello")
+
+#specify a range of numbers. Here, loop through 10 times. Then do what ever is indented on line 2. Here, print Hello
+
+#for number in range(10):
+#  print("Hello")
+#  print("Hi there")
+
+#each time you run the loop "number" holds a different value
+
+#for number in range(10):
+#  print(number) 
+
+#"number" starts at 0, then 1...until range satisfied. Doesn't have to be "number". x is more common
+
+#for x in range(10):
+#  print(x) 
+
+#list and For loops can work together
+
+#fav_movies = ["Sandlot", "The Lego Movie", "Dune"]
+
+#for movie in fav_movies:
+#  print(movie)
+
+#notice that range is just for int
+
+#pretty powerful, the following takes a few seconds
+
+#for x in range(100000):
+#  print(x) 
+
+# Loop 40 times and print the number of the loop times 2. E.G. 2,4,6...
+
+for x in range(40):
+  print(x*2) #0 base counting
+
+for x in range(40):
+  print((x+1)*2) #add 1 to number first
+
+
+
+
+
+
+
+
+
+
+
+
+
